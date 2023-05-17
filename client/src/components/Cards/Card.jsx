f6a4b11fabe744f29eb986e19bba4e89f14c6875
PedroMB22 <104780531+PedroMB22@users.noreply.github.com>
2023-05-17 00:10:24 -0300

    Cards styles and Details button

--- client/src/components/Cards/Card.jsx
+++ client/src/components/Cards/Card.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../../css/cards.css'
 
 const Card = (props) => {
-    const { name, image, temperament, weight, life_span } = props
-  return (
-    <div className="card">
-      <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <p>Temperament: {temperament}</p>
-      <p>Weight: {weight}</p>
-      <p>Life Span: {life_span}</p>
-    </div>
-  )
+    const { id, name, image, temperament, weight, life_span } = props
+    return (
+        <div className="card">
+            <img src={image} alt={name} />
+            <div className="card-text">
+                <h3>{name}</h3>
+                <p>Temperament: {temperament}</p>
+                <p>Weight: {weight}</p>
+                <p>Life Span: {life_span}</p>
+            </div>
+            <Link to={`/dogdetail/${id}`}>
+                <button className="input-box button">Details</button>
+            </Link>
+        </div>
+    )
 }
 
 export default Card
